1320b2a52cd646aab5568568dcb95f97b8abaa71
SuperRedstoneYT <106267340+SuperRedstoneYT@users.noreply.github.com>
2023-09-20 17:47:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log('I have no clue where this logs to. Help.');
+console.log('If anyone can help find where this logs to. I would be very happy.');
+console.log('and yes, i have looked for a console panel'.toUpperCase());
